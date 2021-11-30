b1ca91b06c1873d0e2857e6a349b04c8854f2043
sachinsachdev08 <sachinsachdev28@gmail.com>
2021-11-30 15:29:12 +0530

    Added Homepage

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React , { useEffect } from "react";
 import "./Products.css";
 
 function Products() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="products">
       <h1>Products</h1>
